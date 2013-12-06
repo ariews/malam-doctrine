c7ec155eae24dfdf6320771949afaef30365554d
Arie W. Subagja <arie@malam.or.id>
2013-12-06 14:24:55 +0700

    set output with use $this->response->body() rather than echoing

--- classes/malam/controller/cli/migration.php
+++ classes/malam/controller/cli/migration.php
@@ -8,25 +8,33 @@ defined('SYSPATH') or die('No direct script access.');
 
 abstract class Malam_Controller_Cli_Migration extends Controller
 {
+    protected $_message = '';
+
     public function action_index()
     {
-        echo Malam_Doctrine::info();
+        $this->_message = Malam_Doctrine::info();
     }
 
     public function action_run()
     {
         $version = $this->request->param('version');
-        echo Malam_Doctrine::run($version);
+        $this->_message = Malam_Doctrine::run($version);
     }
 
     public function action_create()
     {
         $name = Arr::get(CLI::options('name'), 'name', NULL);
-        echo Malam_Doctrine::create($name);
+        $this->_message = Malam_Doctrine::create($name);
     }
 
     public function action_reset()
     {
-        echo Malam_Doctrine::reset();
+        $this->_message = Malam_Doctrine::reset();
+    }
+
+    public function after()
+    {
+        $this->response->body($this->_message);
+        parent::after();
     }
 }
